515ba7b0a5758b95afd6062f6c5dc9fdaccd15cb
TiroV <91702935+TiroV@users.noreply.github.com>
2021-10-05 15:14:29 -0700

    Added files
    Committing will update the script file to include a variable update of my favorite food.

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,19 @@
+// let is a way to establish variables. Use lower case and then Upper case.
+let myName = "Tyler";
+document.write(myName);
+alert(myName);
+
+//This is how you write a comment in JavaScript
+
+//Math can be done in JavaScript too. That's cool.
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+let favoriteFood = "Pizza";
+document.write(favoriteFood);
