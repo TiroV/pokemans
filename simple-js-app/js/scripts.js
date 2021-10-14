25a21925f06394cf56138d9281870d44226b196d
TiroV <91702935+TiroV@users.noreply.github.com>
2021-10-14 15:22:10 -0700

    Put PokemonList in IIFE
    If applied, makes the pokemon list a local list and added commands to call on  the array. Added getAll and add.
    
    Added scorbunny to the list of pokemon.

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,38 +1,56 @@
-let pokemonList = [
-  { name: 'Tyranitar',
-    height: 6,
-    types: ['rock', 'dark']
-  },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Tyranitar',
+      height: 6,
+      types: ['rock', 'dark']
+    },
 
-  { name: 'Umbreon',
-    height: 3,
-    types: ['dark']
-  },
+    { name: 'Umbreon',
+      height: 3,
+      types: ['dark']
+    },
 
-  { name: 'Toxicroak',
-    height: 1,
-    types: ['poison', 'fighting']
-  },
+    { name: 'Toxicroak',
+      height: 1,
+      types: ['poison', 'fighting']
+    },
 
-  { name: 'Luxray',
-    height: 2,
-    types: ['electric']
+    { name: 'Luxray',
+      height: 2,
+      types: ['electric']
 
+    }
+
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
 
-];
+  function getAll() {
+    return pokemonList;
+  }
 
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <5 && pokemonList[i].height >2){
-  document.write(pokemonList[i].name + ' is a normal sized pokemon. ');
-}else if(pokemonList[i].height >5){
-  document.write(pokemonList[i].name + ' is a large pokemon. ');
-}else {
-  document.write(pokemonList[i].name + ' is a small pokemon. ');
-}
+  return {
+    add: add,
+    getAll: getAll
 
-for (let i=0; i < pokemonList[i].length; i++){
-  document.write(pokemonList)[i].name; i++
-}
 
-}
+  };
+
+
+})();
+
+pokemonRepository.add({ name: 'Scorbunny' });
+console.log(pokemonRepository.getAll());
+
+
+
+
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+
+
+
+});
