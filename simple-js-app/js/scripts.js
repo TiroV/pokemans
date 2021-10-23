e220a3facfc0e40773fa985c01ae1f27746747d0
TiroV <91702935+TiroV@users.noreply.github.com>
2021-10-22 21:21:31 -0700

    Updated Javascript to include API support
    When applied, adds functionality for the pokemon API.

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,27 +1,16 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: 'Tyranitar',
-      height: 6,
-      types: ['rock', 'dark']
-    },
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    { name: 'Umbreon',
-      height: 3,
-      types: ['dark']
-    },
 
-    { name: 'Toxicroak',
-      height: 1,
-      types: ['poison', 'fighting']
-    },
 
-    { name: 'Luxray',
-      height: 2,
-      types: ['electric']
-
-    }
-
-  ];
+  fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+  return response.json(); // This returns a promise!
+}).then(function (pokemonList) {
+  console.log(pokemonList); // The actual JSON response
+}).catch(function () {
+  // Error
+});
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -39,38 +28,88 @@ function addListItem(pokemon){
   button.classList.add("button-class")
   listPokemon.appendChild(button);
   pokemonList.appendChild(listPokemon);
-  button.addEventListener('click', function () {
+  button.addEventListener('click', function (event) {
   showDetails(pokemon)
-          });
+  });
 }
 
 function showDetails(pokemon){
   console.log(pokemon);
 }
 
+function showLoadingMessage() {
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+  if (response.status != 200)
+  window.alert('This is taking a while...');
+  return;
+}
+
+function hideLoadingMessage() {
+
+
+}
+
+function loadList() {
+  return fetch(apiUrl).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+      console.log(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+
+  })
+}
+
+function loadDetails(item){
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    item.imageUrl = details.sprites.font_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+
+}
+
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
-    showDetails: showDetails
-
+    showDetails: showDetails,
+    loadList: loadList
 
   };
 
 
-})();
-
-pokemonRepository.add({ name: 'Scorbunny', height: 1, types: ['fire'] });
-console.log(pokemonRepository.getAll());
+}) ();
 
 
 
 
 
 
+
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon) {
 
 
 
   pokemonRepository.addListItem(pokemon);
+});
 });
